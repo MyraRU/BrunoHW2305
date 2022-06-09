a19f9bfede6783ab7b126ee36bd72990ae18ee64
MyraRU <myraviev.ivan@gmail.com>
2022-06-09 22:04:28 +0300

    Zalupi4

--- src/HW4/Main.java
+++ src/HW4/Main.java
@@ -1,16 +1,20 @@
 package HW4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        PhoneNumber phoneNumber1 = new PhoneNumber(8800333,"Личный");
+        PhoneNumber phoneNumber1 = new PhoneNumber(9312902018L,"Личный");
 
-        PhoneNumber phoneNumber2 = new PhoneNumber(85553535,"Рабочий");
+        PhoneNumber phoneNumber2 = new PhoneNumber(89210968968L,"Рабочий");
 
+
+        phoneNumber1.print();
         phoneNumber2.print();
-        System.out.println(phoneNumber2.getNum());
-        phoneNumber2.changeType("Домашний");
-        phoneNumber2.print();
+//        System.out.println(phoneNumber2.getNum());
+//        phoneNumber2.changeType("Домашний");
+//        phoneNumber2.print();
 
     }
 }
--- src/HW4/PhoneNumber.java
+++ src/HW4/PhoneNumber.java
@@ -2,30 +2,33 @@ package HW4;
 
 public class PhoneNumber {
 
-    int num;
+    long num;
     String numType;
 
     public PhoneNumber() {
         numType = "";
     }
 
-    public PhoneNumber(int num, String numType) {
-        this.num = num;
+    public PhoneNumber(long num, String numType) {
+        String s = String.valueOf(num);
+
+        if (s.length() == 11 && s.indexOf('8') == 0) {
+            this.num = Long.parseLong(s);
+        } else {
+            System.out.println("Num incorrect");
+        }
         this.numType = numType;
     }
 
-    public void print(){
+    public void print() {
         System.out.println(num + " " + numType);
     }
 
-    public void changeType(String numType){
+    public void changeType(String numType) {
         this.numType = numType;
     }
 
-    public int getNum(){
+    public long getNum() {
         return num;
     }
-
-
-
 }
